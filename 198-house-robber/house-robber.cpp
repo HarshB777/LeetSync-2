2e820909def0befc8ba70d71ed8f2f7d672c3a20
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-07-30 09:58:19 +0530

    Time: 15 ms (31.42%) | Memory: 10.7 MB (6.32%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -26,21 +26,39 @@ int tab(vector<int> nums) {
         return 0;
     }
 
-    // Initialize dp table
-    vector<vector<int>> dp(n + 1, vector<int>(n + 2, 0));
+    vector<vector<int>> dp(n+1, vector<int> (n+2,0));
 
-    for (int idx = n - 1; idx >= 0; idx--) {
-        for (int last = -2; last < idx; last++) {
+    for (int i = n-1; i >= 0; i--)
+    {
+        for (int j = -2; j < i; j++)
+        {
             int m1 = 0;
-            if (idx - last != 1) {
-                m1 = nums[idx] + dp[idx + 1][idx + 2];
+            int bb = 0, cc = 0;
+            if (i - j != 1)
+            {
+                bb = nums[i] + dp[i+1][i+2];
+                //m1 = max(m1,bb);
             }
-            int m2 = dp[idx + 1][last + 2];
-            dp[idx][last + 2] = max(m1, m2);
+            cc = dp[i+1][j+2];
+            m1 = max(bb,cc);
+            dp[i][j+2] = m1;
         }
     }
 
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n+1; j++)
+        {
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
     return dp[0][0];
+
+
+
 }
 
 
