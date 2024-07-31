6c9f8d1558ddf15e31c48599abaa1662cfec0076
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-07-31 11:20:08 +0530

    Time: 20 ms (92.26%) | Memory: 38.4 MB (57.40%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int rp = 1;
         int n = nums.size();
-        vector<int> left(n,1);
-        vector<int> right(n,1);
-
-        
-
-        for (int i = 1; i < n-1; i++)
-        {
-            left[i] = left[i-1]*nums[i-1];
-            right[n-i-1] = right[n-i]*nums[n-i];
-        }
-        left[n-1] = left[n-2]*nums[n-2];
-        right[0] = right[1]*nums[1];
-
-        for (int x: left)
-        {
-            cout<<x<<" ";
-        }
-        cout<<endl;
-        for (int x: right)
-        {
-            cout<<x<<" ";
-        }
-        cout<<endl;
         vector<int> ans(n,1);
-
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-            ans[i] = left[i]*right[i];
+            rp*=nums[i-1];
+            ans[i] *= rp;
+        }
+
+        rp = 1;
+        for (int i = n-2; i >= 0; i--)
+        {
+            rp *= nums[i+1];
+            ans[i] *= rp;
         }
 
         return ans;
