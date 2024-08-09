a99e14a8f50a15f4d4d0864dcf9f92bfe0a0a67e
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-08-09 13:09:07 +0530

    Time: 3 ms (29.51%) | Memory: 7.7 MB (20.43%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    void help(int curr, int n, int &ans)
+    {
+        if (curr == n)
+        {
+            ans++;
+            return;
+        }
+
+        if (curr + 2 <= n)
+        {
+            help(curr+2,n,ans);
+        }
+
+        help(curr+1,n,ans);
+
+
+    }
+
+    int memo(int curr, int n, vector<int> &dp)
+    {
+
+        
+        if (curr == n)
+        {
+            return 1;
+        }
+
+        if (dp[curr] != -1)
+        {
+            return dp[curr];
+        }
+        
+        int two = 0, one = 0;
+
+        if (curr + 2 <= n)
+        {
+            two = memo(curr+2,n,dp);
+        }
+        one = memo(curr+1,n,dp);
+
+        dp[curr] = one + two;
+
+        return dp[curr];
+
+    }
+    int climbStairs(int n) {
+        int ans = 0;
+        //recursive solutions
+        //help(0, n, ans);
+        //return ans;
+
+
+        //memoization
+        vector<int> dp(n,-1);
+        return memo(0, n, dp);
+    }
+};
