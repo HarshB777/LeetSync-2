f4c344ba6334dedd81aa461eb130dfef9365f912
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2024-08-09 13:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (6.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -53,7 +53,18 @@ public:
 
 
         //memoization
-        vector<int> dp(n,-1);
-        return memo(0, n, dp);
+        //vector<int> dp(n,-1);
+        //return memo(0, n, dp);
+
+        //tabulation
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
