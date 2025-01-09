0fbc61007c1bd2aaed20c3c2183f0fb073c05db3
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2025-01-09 21:18:31 +0530

    Time: 3 ms (6.94%) | Memory: 9.5 MB (7.33%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
-    int xf, yf;
-    void help(int x, int y, int &ans)
+    int tab(int m, int n)
     {
-        if (x == xf-1 && y == yf-1)
-        {
-            ans++;
-            return;
-        }
-        if (x >= xf || y >= yf)
-        {
-            return;
-        }
-        help(x + 1, y, ans);
-        help(x, y + 1, ans);
-    }
+        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
+        dp[m-1][n-1] = 1;
 
-    int memo(int x, int y, vector<vector<int>> &dp)
-    {
-        if (x == xf - 1 && y == yf -1)
+        for (int i = m-1; i >= 0; i--)
         {
-            cout<<"returning 1"<<endl;
-            return 1;
+            for (int j = n-1; j >= 0; j--)
+            {
+                if (!(i == m-1 && j == n-1))
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
         }
-        if (x >= xf || y >= yf)
-        {
-            return 0;
-        }
-
-        if (dp[x][y] != -1)
-        {
-            return dp[x][y];
-        }
-
-        int right = 0, down = 0;
-        right = memo(x, y + 1, dp);
-        down = memo(x + 1, y, dp);
-
-        return dp[x][y] = right + down;
-        
+        return dp[0][0];
     }
     int uniquePaths(int m, int n) {
-        int x = 0, y = 0;
-        // int ans = 0;
-
-        xf = m;
-        yf = n;
-
-        // help(x,y,ans);
-
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-
-        return memo(x,y,dp);
+        return tab(m,n);
     }
 };
