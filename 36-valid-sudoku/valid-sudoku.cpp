c051e814f6d1552f3b8f768503b5d3bc0549f41e
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2025-04-18 16:52:32 +0530

    Time: 4 ms (53.41%) | Memory: 23.1 MB (62.26%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,48 +1,75 @@
 class Solution {
 public:
+
     bool isValidSudoku(vector<vector<char>>& board) {
-        int r = board.size();
-        int c = board[0].size();
+        int rows = board.size();
+        int cols = board[0].size();
 
-        vector<vector<bool>> rows(r, vector<bool> (10,false));
-        vector<vector<bool>> cols(c, vector<bool> (10,false));
-        vector<vector<bool>> sub(9, vector<bool> (10,false));
+        if ((rows / 3) != (cols / 3)) {
+            return false;
+        }
+        
+        // number of individual 3*3 matrix
+        int numEachMatrix = 9;
 
-        for (int i = 0; i < r; i++)
-        {
-            for (int j = 0; j < c; j++)
-            {
-                int num = -1;
-                if (board[i][j] != '.')
-                {
-                    num = board[i][j]-'0';
-                }
 
-                if (num != -1)
-                {
-                    if (rows[i][num])
-                        return false;
-                    
-                    rows[i][num] = true;
+        // tracker for individual matrix as to 1-9 is visited
+        vector<vector<bool>> eachMatrix(numEachMatrix, vector<bool> (10,false));
 
-                    if (cols[j][num])
-                        return false;
-                    cols[j][num] = true;
-                    
-                    int sub_idx = 0;
-                    sub_idx = (i/3)*3 + j/3;
-                    if (sub[sub_idx][num])
-                    {
-                        return false;
-                    }
-                    sub[sub_idx][num] = true;
-                }
-                //endof if
+        // tracker for individual rows as to 1-9 is visited
+        vector<vector<bool>> eachRow(rows, vector<bool> (10, false));
+
+        // tracker for individual cols as to 1-9 is visited
+        vector<vector<bool>> eachCol(cols, vector<bool> (10, false));
+
+        bool ansFlag = true;
+        for (int curr_row = 0; curr_row < rows; curr_row++) {
+            for (int curr_col = 0; curr_col < cols; curr_col++) {
                 
+                int index_eachMatrix = (curr_row / 3)*3 + curr_col / 3;
+                int index_eachRow = curr_row;
+                int index_eachCol = curr_col;
+                if (board[curr_row][curr_col] == '.') {
+                    continue;
+                }
+                int curr_value = board[curr_row][curr_col] - '0';
+
+                // check if curr_value is visited in eachMatrix
+                if (eachMatrix[index_eachMatrix][curr_value])
+                {
+                    cout << "breaking for eachMatrix :: r = " << curr_row << " c = " << curr_col << " curr_val = ";
+                    cout << curr_value << " bool = "<< eachMatrix[index_eachMatrix][curr_value] << endl;
+                    ansFlag = false;
+                    break;
+                }
+
+                // check if curr_value is visited in eachRow
+                if (eachRow[index_eachRow][curr_value]){
+                    cout << "breaking for eachRow :: r = " << curr_row << " c = " << curr_col << endl;
+                    ansFlag = false;
+                    break;
+                }
+
+                // check if curr_value is visited in eachCol
+                if (eachCol[index_eachCol][curr_value]) {
+                    ansFlag = false;
+                    cout << "breaking for eachRow :: r = " << curr_row << " c = " << curr_col << endl;
+                    break;
+                }
+
+
+                // if control comes here means it is not visited yet, in all possiblities. mark visited in all 3 possiblites
+                eachMatrix[index_eachMatrix][curr_value] = true;
+                eachRow[index_eachRow][curr_value] = true;
+                eachCol[index_eachCol][curr_value] = true;
             }
-            
+            if (!ansFlag)
+            {
+                cout << "::::::: break call received breaking " << endl << endl;
+                break;
+            }
         }
 
-        return true;
+        return ansFlag;
     }
 };
