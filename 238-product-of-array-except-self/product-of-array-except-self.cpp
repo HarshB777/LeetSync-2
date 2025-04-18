cead0e050dd1314c6cc3cb675f3fd440751d83af
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2025-04-18 16:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (16.64%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
+    int sz;
+    vector<int> ans;
+    vector<int> vec;
+
+
+    void computeProductExceptSelf() {
+        ans.resize(sz, 1);
+        
+        // multiply element before it
+        int leftProduct = 1;
+        for (int i = 1; i < sz; i++)
+        {
+            leftProduct *= vec[i-1];
+            ans[i] *= leftProduct;
+        }
+        
+        // multiply element after it
+        int rightProduct = 1;
+        for (int i = sz-2; i >= 0; i--)
+        {
+            rightProduct *= vec[i+1];
+            ans[i] *= rightProduct;
+        }
+    }
+
+
     vector<int> productExceptSelf(vector<int>& nums) {
-        int rp = 1;
-        int n = nums.size();
-        vector<int> ans(n,1);
-        for (int i = 1; i < n; i++)
-        {
-            rp*=nums[i-1];
-            ans[i] *= rp;
-        }
+        vec = nums;
+        sz = nums.size();
 
-        rp = 1;
-        for (int i = n-2; i >= 0; i--)
-        {
-            rp *= nums[i+1];
-            ans[i] *= rp;
-        }
-
+        computeProductExceptSelf();
         return ans;
     }
 };
