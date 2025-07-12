625c55d049c763e27248be57817acb8123a071b1
HarshB777 <96071876+HarshB777@users.noreply.github.com>
2025-07-12 13:49:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (57.31%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int tab(int m, int n)
-    {
-        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
-        dp[m-1][n-1] = 1;
+    vector<vector<int>> dp;
+    int ans;
 
-        for (int i = m-1; i >= 0; i--)
-        {
-            for (int j = n-1; j >= 0; j--)
-            {
-                if (!(i == m-1 && j == n-1))
-                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
-            }
+    int memo(int x, int y, int m, int n) {
+
+        if (x == m && y == n) {
+            return 1;
         }
-        return dp[0][0];
+        if (x > m || y > n) {
+            return 0;
+        }
+
+        if (dp[x][y] != -1) {
+            return dp[x][y];
+        }
+        
+
+        return dp[x][y] = memo(x+1,y,m,n) + memo(x,y+1,m,n);
     }
+
+
+
     int uniquePaths(int m, int n) {
-        return tab(m,n);
+        dp.resize(m, vector<int> (n,-1));
+        dp[m-1][n-1]=1;
+        
+        
+        return memo(0,0,m-1,n-1);
     }
 };
